d3763f23a63afa754f4b1743489a16618c376148
smkrielt <smkrielt@gmail.com>
2018-02-27 18:16:07 +0200

    :elephant: renamed environment variables

--- 3.6/start.sh
+++ 3.6/start.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
+USER=${COMPOSE_MONGODB_ADMIN_NAME:-"admin"}
+PASS=${COMPOSE_MONGODB_ADMIN_PASS:-"admin"}
 DATABASE=${COMPOSE_MONGODB_DATABASE:-"admin"}
-USER=${COMPOSE_MONGODB_USER:-"admin"}
-PASS=${COMPOSE_MONGODB_PASS:-"admin"}
 AUTH=${COMPOSE_MONGODB_AUTH:-"yes"}
 ENGINE=${COMPOSE_MONGODB_STORAGE_ENGINE:-"wiredTiger"}
 TIMEZONE=${LYBERTEAM_TIME_ZONE:-"Europe/Kiev"}
 
 add_user() {
     echo "---> Set  user: ${USER} with password: ${PASS}"
-    mongo admin --eval "db.createUser({user: '$USER', pwd: '$PASS', roles:[{role:'root',db:'$DATABASE'}]});"
+    mongo admin --eval "db.createUser({user: '$USER', pwd: '$PASS', roles:[{role:'root',db:'admin'}]});"
 
 if [ "$DATABASE" != "admin" ]; then
     echo "---> Set  user: ${USER} with password: ${PASS}"
