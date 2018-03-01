e3152a169ae7b5022d00358c9959b38dcfe07efc
smkrielt <smkrielt@gmail.com>
2018-03-01 16:01:39 +0200

    :elephant: fixed bugs. changed environment variables

--- 3.6/start.sh
+++ 3.6/start.sh
@@ -1,42 +1,44 @@
 #!/bin/bash
-USER=${COMPOSE_MONGODB_ADMIN_NAME:-"admin"}
-PASS=${COMPOSE_MONGODB_ADMIN_PASS:-"admin"}
-DATABASE=${COMPOSE_MONGODB_DATABASE:-"admin"}
-AUTH=${COMPOSE_MONGODB_AUTH:-"yes"}
+ADMIN_USER=${COMPOSE_MONGODB_ADMIN_NAME:-""}
+ADMIN_PASS=${COMPOSE_MONGODB_ADMIN_PASS:-""}
+USER=${COMPOSE_MONGODB_USER_NAME:-""}
+PASS=${COMPOSE_MONGODB_USER_PASS:-""}
+DATABASE=${COMPOSE_MONGODB_DATABASE:-""}
+AUTH=${COMPOSE_MONGODB_AUTH:-"no"}
 ENGINE=${COMPOSE_MONGODB_STORAGE_ENGINE:-"wiredTiger"}
 TIMEZONE=${LYBERTEAM_TIME_ZONE:-"Europe/Kiev"}
 
 add_user() {
-    echo "---> Set  user: ${USER} with password: ${PASS}"
-    mongo admin --eval "db.createUser({user: '$USER', pwd: '$PASS', roles:[{role:'root',db:'admin'}]});"
+if [ "$COMPOSE_MONGODB_ADMIN_NAME" != "" ]; then
+    mongo admin --eval "db.createUser({user: '$ADMIN_USER', pwd: '$ADMIN_PASS', roles:[{role:'userAdminAnyDatabase',db:'admin'}]});"
 
-if [ "$DATABASE" != "admin" ]; then
-    echo "---> Set  user: ${USER} with password: ${PASS}"
-    mongo admin -u $USER -p $PASS << EOF
-use $DATABASE
-db.createUser({user: '$USER', pwd: '$PASS', roles:[{role:'dbOwner',db:'$DATABASE'}]})
+    if [ "$DATABASE" != "admin" ]; then
+        echo "---> Set  user: ${USER} with password: ${PASS}"
+        mongo  admin -u $ADMIN_USER -p: $ADMIN_PASS --authenticationDatabase "admin" << EOF
+use $DATABASE;
+db.createUser({user: '$USER', pwd: '$USER', roles:[ { role: "readWrite", db: "$DATABASE" },
+             { role: "read", db: "reporting" } ]});
 EOF
-fi
+    fi
 
     echo "---> User successfully added!"
     touch /data/db/.user_is_set
+fi
 }
+
+if [ ! -e /data/db/.user_is_set ]; then
+    add_user
+fi
 
 # Add default timezone
 echo "$TIMEZONE" > /etc/timezone
-echo "date.timezone=$TIMEZONE" > /etc/php/7.0/cli/conf.d/timezone.ini
 dpkg-reconfigure -f noninteractive tzdata
 
 set -m
-
-COMMAND="mongod --storageEngine $ENGINE  --master"
+COMMAND="mongod --bind_ip_all --storageEngine $ENGINE"
 
 if [ "$AUTH" == "yes" ]; then
     COMMAND="$COMMAND --auth"
 fi
 
 $COMMAND
-
-if [ ! -f /data/db/.user_is_set ]; then
-    add_user
-fi
--- README.md
+++ README.md
@@ -3,7 +3,7 @@
  **image from debian:jessie**
 
 ## Tags
-   v3.6 ([3.6/Dockerfile](https://github.com/lyberteam/mongo/tree/master/3.6))
+   tag - 3.6 ( [3.6/Dockerfile](https://github.com/lyberteam/mongo/tree/master/3.6) )
 
 
 ## Using with docker-compose
@@ -16,12 +16,18 @@
             container_name: example_mongo
             image: lyberteam/mongo:3.6
             ports:
-                - 27017:27017
+                - 27018:27017
             environment:
-                MONGODB_DATABASE: example_db
-                MONGODB_USER: example_user
-                MONGODB_PASS: example_pass
-                MONGODB_AUTH: yes
-                MONGODB_STORAGE_ENGINE: wiredTiger
-                LYBERTEAM_TIME_ZONE: Europe/Kiev
+                COMPOSE_MONGODB_ADMIN_NAME: 'admin_user'
+                COMPOSE_MONGODB_ADMIN_PASS: 'admin_pass'
+                COMPOSE_MONGODB_USER_NAME: 'user'
+                COMPOSE_MONGODB_USER_PASS: 'pass'
+                COMPOSE_MONGODB_DATABASE: 'example_db'
+                COMPOSE_MONGODB_AUTH: 'yes'
+                COMPOSE_MONGODB_STORAGE_ENGINE: 'wiredTiger'
+                LYBERTEAM_TIME_ZONE: 'Europe/Kiev'
             working_dir: /data/db
+            volumes:
+                - ./mongo_files/db:/data/db
+                - ./mongo_files/configdb:/data/configdb
+            command: ["/start.sh"]
